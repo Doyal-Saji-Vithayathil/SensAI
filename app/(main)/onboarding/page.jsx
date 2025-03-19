80b7619f0190de0d5d1f87bd09df85271af80922
Doyal Saji Vithayathil <150351001+Doyal-Saji-Vithayathil@users.noreply.github.com>
2025-03-19 14:47:41 +0530

    Onboarding Page

---
+++ app/(main)/onboarding/page.jsx
@@ -0,0 +1,12 @@
+import { industries } from "@/data/industries";
+
+const OnboardingPage = () => {
+  //Check if user is already onboarded
+  return (
+    <main>
+      <OnboardingForm industries={industries}/>
+    </main>
+  );
+};
+
+export default OnboardingPage;
