ceefd36f9adac9828ed4075719908b619c162664
Doyal Saji Vithayathil <150351001+Doyal-Saji-Vithayathil@users.noreply.github.com>
2025-03-21 11:44:52 +0530

    Onboarding APIs

--- app/(main)/onboarding/_components/onboarding-form.jsx
+++ app/(main)/onboarding/_components/onboarding-form.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+"use client";
+
+import { useForm } from "react-hook-form";
 
 const OnboardingForm = ({industries}) => {
+  useForm()
   return (
     <div>OnboardingForm</div>
   )
--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,10 +1,18 @@
+import { getUserOnboardingStatus } from "@/actions/user";
 import { industries } from "@/data/industries";
+import { redirect } from "next/navigation";
 
-const OnboardingPage = () => {
+
+const OnboardingPage = async () => {
   //Check if user is already onboarded
+  const { isOnboarded } = await getUserOnboardingStatus();
+  if (isOnboarded) {
+    redirect("/dashboard");
+  }
+
   return (
     <main>
-      <OnboardingForm industries={industries}/>
+      <OnboardingForm industries={industries} />
     </main>
   );
 };
