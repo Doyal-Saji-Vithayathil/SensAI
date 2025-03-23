23a240a4ff53a23c71462c0a216f136c363a015b
Doyal Saji Vithayathil <150351001+Doyal-Saji-Vithayathil@users.noreply.github.com>
2025-03-23 10:50:03 +0530

    Interview Preparation Page,Generate Mock Interview Questions with AI

--- app/(main)/interview/mock/page.jsx
+++ app/(main)/interview/mock/page.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 const MockInterviewPage = () => {
   return (
-    <div>MockInterviewPage</div>
-  )
-}
+    <div>
+      <div className="flex flex-col space-y-2 mx-2">
+        <Link href={"/interview"}>
+          <Button variant="link" className="gap-2 pl-0">
+            <ArrowLeft className="h-4 w-4" />
+            Back to Interview Preparation
+          </Button>
+        </Link>
 
-export default MockInterviewPage
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MockInterviewPage;
